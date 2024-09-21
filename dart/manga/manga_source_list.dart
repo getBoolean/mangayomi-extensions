0c37dbd62d16cbc5e4fb67a8d404bac7eab68759
getBoolean <19920697+getBoolean@users.noreply.github.com>
2024-09-21 17:28:15 -0500

    Revert "remove some NSFW sources"
    This reverts commit 24825d0d9c8d3a4f9b7a24a684116edb52092d2c.

--- dart/manga/manga_source_list.dart
+++ dart/manga/manga_source_list.dart
@@ -1,4 +1,5 @@
 import '../../model/source.dart';
+import 'multisrc/heancms/sources.dart';
 import 'multisrc/madara/sources.dart';
 import 'multisrc/mangabox/sources.dart';
 import 'multisrc/mangareader/sources.dart';
@@ -7,6 +8,7 @@ import 'multisrc/nepnep/sources.dart';
 import 'src/all/batoto/sources.dart';
 import 'src/all/comick/sources.dart';
 import 'src/all/mangadex/sources.dart';
+import 'src/all/nhentai/sources.dart';
 import 'src/en/mangahere/source.dart';
 
 List<Source> dartMangasourceList = [
@@ -15,8 +17,10 @@ List<Source> dartMangasourceList = [
   ...mangaDexSourcesList,
   ...mangareaderSourcesList,
   ...mmrcmsSourcesList,
+  ...heancmsSourcesList,
   mangahereSource,
   ...nepnepSourcesList,
   ...mangaboxSourcesList,
-  ...batotoSourcesList
+  ...batotoSourcesList,
+  ...nhentaiSourcesList
 ];
